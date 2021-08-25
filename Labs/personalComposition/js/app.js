346892270f1f9bf99b15728e64e2bdc3cee67a97
weems123 <47086757+weems123@users.noreply.github.com>
2021-08-25 18:00:22 -0400

    Adding Lab One

---
+++ Labs/personalComposition/js/app.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(600, 600);
+  var purple ="#b103fc";
+  var noseMouth = "3";
+
+  background(26, 27, 28);
+
+  noStroke();
+  fill(purple);
+  ellipse(450, 90, 50);
+
+  stroke(3, 202, 252);
+  strokeWeight(noseMouth);
+  
+  fill(200, 50, 50);
+  rect(50, 75, 30, 30);
+
+  strokeWeight(noseMouth);
+  line(150, 350, 375, 400);
+  line(200, 250, 250, 250);
+  line(200, 250, 250, 190);
+
+
+
+}
