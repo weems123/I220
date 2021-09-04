9d97140be9ca6ffac1d9b051eaeb8f92cc2aac45
weems123 <47086757+weems123@users.noreply.github.com>
2021-09-04 12:10:02 -0400

    final updates for week 2

--- Labs/PuckSide/js/app.js
+++ Labs/PuckSide/js/app.js
@@ -1,25 +1,20 @@
-//variable for where to start the circle
-var xSpot = 0;
 
 //needed to start the code for the program (must come before draw)
 function setup() {
     createCanvas(400,300);
-
 }
 
 //starting function of draw that runs 60 times per second
 function draw() {
 //putting background first so it's just a single circle
-    background(255, 255, 255);
+    background(200, 200, 200);
     //mouse follows the cursor with a radius of 50
-        ellipse(mouseX, mouseY, 50);
-
+    ellipse(mouseX, mouseY, 50);
 //color of the circle(blue)
     fill(27, 45, 181);
   
 //IF mouse moves over the threshold of 200px then change color to RED    
     if(mouseX > 200) {
         fill(247,7,47);
-    }
-    
+    }   
 }
