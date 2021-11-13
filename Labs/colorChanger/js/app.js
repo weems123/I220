502ac64b855773ecdf42527fe0c8b91310fcd805
weems123 <47086757+weems123@users.noreply.github.com>
2021-11-13 12:31:35 -0500

    week

--- Labs/colorChanger/js/app.js
+++ Labs/colorChanger/js/app.js
@@ -1,8 +1,10 @@
-function changeColor(event){
-       //event.target.style.backgroundColor = "red";
 
+//function to change the color depending on which square was pressed
+function changeColor(event){
+
+//let response be the click from the user that changes the square to the color we set in the attribute
        let response = event.target.getAttribute("data-color");
        console.log();
-       //event.target.innerHTML = response;
+
         event.target.style.backgroundColor = response;
    }
--- Labs/colorMixer/js/app.js
+++ Labs/colorMixer/js/app.js
@@ -1,16 +1,50 @@
+
+//setting the random colors for Red Green and Blue
 let r = Math.round(Math.random() * 255);
 let g = Math.round(Math.random() * 255);
 let b = Math.round(Math.random() * 255);
 
+//setting the RGB color to the Red Green and Blue that we established above
 let rgbColor = "rgb(" + r + "," + g + "," + b + ")";
 
 document.getElementById("colorBox").style.backgroundColor = rgbColor;
+document.getElementById("rgb").innerHTML = "rgb(" + r + "," + g + "," + b + ")";
 
+
+//function to change the color depending on which button the user selected
 function changeColor(event){
     let v = event.target.getAttribute("data-value");
+    let color = event.target.getAttribute("data-color");
     v = parseInt(v);
-    r = r + v;
+    
+
+// if statement to see     
+    if(color == "red" && r<255){
+        r += v
+//if the color goes above 255, set it back to 255        
+        if(r > 255){
+            r = 255
+        }
+    }
+    else if(color == "green" && g<255){
+        g += v
+//if the color goes above 255, set it back to 255         
+        if(g > 255){
+            g = 255
+        }
+    }
+    else if(color == "blue" && b<255){
+        b += v
+//if the color goes above 255, set it back to 255         
+        if(b > 255){
+            b = 255
+        }
+    }
+
     rgbColor = "rgb(" + r + "," + g + "," + b + ")";
+
     document.getElementById("colorBox").style.backgroundColor = rgbColor;
+    
+    document.getElementById("rgb").innerHTML = "rgb(" + r + "," + g + "," + b + ")";
 
 }
